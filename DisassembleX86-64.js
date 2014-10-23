6c9d22204dd17227891c61955e23eff4ccb4072a
Damian Recoskie <damian_programmer@hotmail.com>
2014-10-22 18:28:02 -0700

    Full support for rex prefix.
    Full support for rex prefix only for current supported operand types.

--- DisassembleX86-64.js
+++ DisassembleX86-64.js
@@ -98,7 +98,8 @@ n //INC,DEC,CALL,CALLF,JMP,JMPF,PUSH
 //5=Accumulator imm8 input
 //6=Accumulator imm32 input
 //7=Register selection last 3 bits of op code
-//8=no supported operand decode of operation yet
+//8=Rex Prefix opcode
+//9=no supported operand decode of operation yet
 
 var OpcodeOperandType=
 [
@@ -110,72 +111,97 @@ var OpcodeOperandType=
 1,2,3,4,5,6,0,0,
 1,2,3,4,5,6,0,0,
 1,2,3,4,5,6,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
 7,7,7,7,7,7,7,7,
 7,7,7,7,7,7,7,7,
 0,0,0,
-8,
+9,
 0,0,0,0,
-8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
 0,0,0,0,0,
 1,2,1,2,1,2,3,4,
-8,8,8,8,
+9,9,9,9,
 7,7,7,7,7,7,7,7,
-8,8,
+9,9,
 0,
 0,0,0,0,0,
-8,8,8,8,8,8,8,8,
+9,9,9,9,9,9,9,9,
 5,6,
-8,8,8,8,8,8,
+9,9,9,9,9,9,
 
-8,8,8,8,8,8,8,8, //MOV R imm8
-8,8,8,8,8,8,8,8, //MOV R imm8
+9,9,9,9,9,9,9,9, //MOV R imm8
+9,9,9,9,9,9,9,9, //MOV R imm8
 
-8,8,
-8, //RETN imm16
+9,9,
+9, //RETN imm16
 0, //RETN
 0,0,
-8,8,
-8,//enter
+9,9,
+9,//enter
 0,
-8,//retf imm16
+9,//retf imm16
 0,
 0,//int 3
-8, //int imm8
+9, //int imm8
 0, //into
 0, //IRET
 0,0,0,0,0,0,0,
-8,8,8,8,8,8,8,8,8,
-8,8,8,8, //loops
-8,8,8,8, //input output
-8,8, //call jump
+9,9,9,9,9,9,9,9,9,
+9,9,9,9, //loops
+9,9,9,9, //input output
+9,9, //call jump
 0,
-8, //jmp
-8,8,8,8, //input output
-8, //lock
+9, //jmp
+9,9,9,9, //input output
+9, //lock
 0, //reserved
-8,8, //repeat
+9,9, //repeat
 0,0,
-8,8,
+9,9,
 0,0,0,0,0,0, //flags
-8,8
+9,9
 ]
 
 //********************************registers and position in binary code********************************
 
-var Pos=0;
-var RexPrifix=2; //this changes based on the rex operation it is an public variable but resets after the next operation
+var Pos=0; //the position in the binary code
+
+var Rex=[0,0,0,0,0]; //the rex prefix
+
+//RAM ptr size
 
 PTRS=["BYTE PTR [","WORD PTR [","DWORD PTR [","QWORD PTR ["];
 
-var REG=
+REG=[
+
+//normal high low 8 bit registers plus the new extend rex prefix registers
+
 [
-["AL","CL","DL","BL","AH","CH","DH","BH"],
-["AX","CX","DX","BX","SP","BP","SI","DI"],
-["EAX","ECX","EDX","EBX","ESP","EBP","ESI","EDI"],
-["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI"],
-["ES","CS","SS","DS"]
+["AL","CL","DL","BL","AH","CH","DH","BH","R8B","R9B","R10B","R11B","R12B","R13B","R14B","R15B"],
+
+//the new 8 bit register access though rex prefix
+
+["AL","CL","DL","BL","SPL","BPL","SIL","DIL","R8B","R9B","R10B","R11B","R12B","R13B","R14B","R15B"]
+],
+
+//the 16 bit registers plus the new extend rex prefix registers
+
+["AX","CX","DX","BX","SP","BP","SI","DI","R8W","R9W","R10W","R11W","R12W","R13W","R14W","R15W"],
+
+//normal 32 bit registers plus the new extend rex prefix registers
+
+["EAX","ECX","EDX","EBX","ESP","EBP","ESI","EDI","R8D","R9D","R10D","R11D","R12D","R13D","R14D","R15D"],
+
+//normal 64 bit registers plus the new extend rex prefix registers
+
+["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI","R8","R9","R10","R11","R12","R13","R14","R15"]
 ];
+
+//segment registers
+
+SEG=["ES","CS","SS","DS"];
+
+//SIB byte scale
 
 var scale=["","*2","*4","*8"];
 
@@ -193,7 +219,7 @@ function ReadInput(n,p)
 
 var h="";t="";
 
-//return nuthing
+//return nothing
 
 if(n==0){return("");}
 
@@ -212,13 +238,119 @@ if(p){return("+"+h.toUpperCase());}else{return(h.toUpperCase());}}
 
 //********************************decode the Operands for the ModRM and SIB********************************
 
-function DecodeModRM(Data,size){var output1="",output2="",ModR_M=ModRM(Data[Pos]);
-output2=REG[size][ModR_M[1]];if(ModR_M[0]==3){output1=REG[size][ModR_M[2]];}
-else{if(ModR_M[0]==0&ModR_M[2]==5){output1=PTRS[size]+ReadInput(2,0)+"]";}
-else{output1+=PTRS[size];if(ModR_M[2]==4){MulM_M=ModRM(Data[Pos]);
-output1+=REG[3][MulM_M[2]];if(MulM_M[1]!=4){output1+="+"+REG[3][MulM_M[1]]+scale[MulM_M[0]];}}
-else{output1+=REG[3][ModR_M[2]];};output1+=ReadInput(ModR_M[0],1)+"]";}}
-return([output1,output2]);}
+//note type input meaning
+//type=0 input is op1=r/m8,op2=r8
+//type=1 input is op1=r/m8/16/32/64,op2=r8/16/32/64
+
+function DecodeModRM(Data,type)
+{
+
+//Rex values note
+//Rex[0]=B
+//Rex[1]=X
+//Rex[2]=R
+//Rex[3]=W
+//Rex[4]=rex prefix is active
+
+var output1="",output2="",ModR_M=ModRM(Data[Pos]);
+
+var IndexExtend=0,BaseExtend=0,RExtend=0;
+
+//64 bit cpu defaults to 32 bit
+
+var RegGroup=2;
+
+//check if operation is r/m8,r8 only
+
+if(type==0){RegGroup=0;}
+
+//the selection between using the new 8 bit register access and high low
+
+var Reg8Group=0;
+
+//if Rex is active
+
+if(Rex[4]){Reg8Group=1;}
+
+//check if rex is active and check Base and Index extend and register extend
+
+if(Rex[4]&Rex[0]){BaseExtend=8;}
+if(Rex[4]&Rex[1]){IndexExtend=8;}
+if(Rex[4]&Rex[2]){RExtend=8;}
+
+//Check if 64 oprand prefix only if type ModRM type can go 64
+
+if(Rex[4]&Rex[3]&type!=0){RegGroup=3;}
+
+//decode the ModRM and SIB
+
+//decode Reg bit field check if Reg 8
+
+if(RegGroup==0){output2=REG[RegGroup][Reg8Group][ModR_M[1]+RExtend];}
+
+//else normal reg group
+
+else{output2=REG[RegGroup][ModR_M[1]+RExtend];}
+
+//decode the Mode and Memory displacements
+
+if(ModR_M[0]==3)
+{
+
+//check if Reg 8
+
+if(RegGroup==0){output1=REG[RegGroup][Reg8Group][ModR_M[1]+BaseExtend];}
+
+//else normal reg group
+
+else{output1=REG[RegGroup][ModR_M[1]+BaseExtend];}
+
+}
+else
+{
+
+if(ModR_M[0]==0&ModR_M[2]==5)
+{
+output1=PTRS[RegGroup]+ReadInput(2,0)+"]";
+}
+
+else
+{
+
+output1+=PTRS[RegGroup];
+
+if(ModR_M[2]==4)
+{
+//decode the SIB byte
+
+SIB=ModRM(Data[Pos]);
+
+output1+=REG[3][SIB[2]+BaseExtend];
+
+if(SIB[1]!=4)
+{
+output1+="+"+REG[3][SIB[1]+IndexExtend]+scale[SIB[0]];
+}
+
+}
+else
+{
+output1+=REG[3][ModR_M[2]+BaseExtend];
+};
+
+output1+=ReadInput(ModR_M[0],1)+"]";
+}
+
+}
+
+//deactivate Rex prefix
+
+Rex[4]=0;
+
+//return oprands output
+
+return([output1,output2]);
+}
 
 //********************************decode the Mod_R_M byte and SIB********************************
 
@@ -229,10 +361,12 @@ function ModRM(v){Mode=(v>>6)&0x3;R=(v>>3)&0x07;M=v&0x07;Pos+=1;return([Mode,R,M
 function Decode(Data)
 {
 
-type=OpcodeOperandType[Data[Pos]];
-CodeName=opcodes[Data[Pos]];
+value=Data[Pos];
 
-Pos++;
+type=OpcodeOperandType[value];
+CodeName=opcodes[value];
+
+Pos++; //after the opcode byte is read move to the next byte code
 
 if(type==0)
 {
@@ -247,7 +381,7 @@ return(CodeName+o[0]+","+o[1]+"\r\n");
 
 else if(type==2)
 {
-var o=DecodeModRM(Data,RexPrifix);
+var o=DecodeModRM(Data,1);
 return(CodeName+o[0]+","+o[1]+"\r\n");
 }
 
@@ -259,7 +393,7 @@ return(CodeName+o[1]+","+o[0]+"\r\n");
 
 else if(type==4)
 {
-var o=DecodeModRM(Data,RexPrifix);
+var o=DecodeModRM(Data,1);
 return(CodeName+o[1]+","+o[0]+"\r\n");
 }
 
@@ -270,16 +404,51 @@ return(CodeName+REG[0][0]+","+ReadInput(1,0)+"\r\n");
 
 else if(type==6)
 {
-return(CodeName+REG[RexPrifix][0]+","+ReadInput(2,0)+"\r\n");
+return(CodeName+REG[2][0]+","+ReadInput(2,0)+"\r\n");
 }
+
+//last 3 bit of opcode is register selection
 
 else if(type==7)
 {
-return(Opcodes[Data[Pos]&0xF8]+REG[RexPrifix]+"\r\n");
+//settings for if rex prefix is used
+
+var Extend=0;
+
+//check Rex Settings
+
+if(Rex[4]&Rex[0]){Extend=8;}
+
+//deactivate rex
+
+Rex[4]=0;
+
+return(opcodes[value&0xF8]+REG[3][(value&0x07)+Extend]+"\r\n");
 }
-//else does not know how to decode operand but knows the operation code
+
+//if rex prefix
 
 else if(type==8)
+{
+value=(value&0x0F);
+
+Rex=[value&0x01,(value&0x02)>>1,(value&0x04)>>2,(value&0x08)>>3];
+
+//Rex[0]=B
+//Rex[1]=X
+//Rex[2]=R
+//Rex[3]=W
+
+//set rex prfix active
+
+Rex[4]=1;
+
+return("");
+}
+
+//else does not know how to decode operand but knows the operation code
+
+else if(type==9)
 {
 return(opcodes[Data[Pos]]+"?\r\n");Pos++;
 }
