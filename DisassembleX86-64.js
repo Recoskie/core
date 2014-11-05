268ecf33dae021c68c98ec5e3f03464a615b8950
Damian Recoskie <damian_programmer@hotmail.com>
2014-11-05 02:53:20 -0800

    Update DisassembleX86-64.js

--- DisassembleX86-64.js
+++ DisassembleX86-64.js
@@ -14,7 +14,7 @@ for(var i=0;i<t.length;Code[i]=parseInt(t[i],2)&0xFF,i++);
 
 var invalid="Op-code is not an valid 64 bit instruction!";
 var tb="Tow Byte Instructions not Supported yet!"
-var fpu="Float Point unit Not Supported yet!";
+var InvalidFPU="Invalid FPU Operation!";
 
 var opcodes=["ADD ","ADD ","ADD ","ADD ","ADD ","ADD ",invalid,invalid,
 "OR ","OR ","OR ","OR ","OR ","OR ",invalid,tb,
@@ -24,121 +24,115 @@ var opcodes=["ADD ","ADD ","ADD ","ADD ","ADD ","ADD ",invalid,invalid,
 "SUB ","SUB ","SUB ","SUB ","SUB ","SUB ",invalid,invalid,
 "XOR ","XOR ","XOR ","XOR ","XOR ","XOR ",invalid,invalid,
 "CMP ","CMP ","CMP ","CMP ","CMP ","CMP ",invalid,invalid,
-
-"", "", "", "", "", "", "", "",
-"", "", "", "", "", "", "", "",
-
-"PUSH ", "", "", "", "", "", "", "",
-"POP ", "", "", "", "", "", "", "",
-
+,,,,,,,,,,,,,,,,
+"PUSH ",,,,,,,,
+"POP ",,,,,,,,
 invalid ,invalid,invalid,
-
 "MOVSXD ",
-
-"FS:","GS:",
-
-"","",
-
+"FS:","GS:",,,
 "PUSH ","IMUL ","PUSH ","IMUL ",
 "INS ","INS ","OUTS ","OUTS ",
 "JO ","JNO ","JB ","JAE ","JE ","JNE ","JBE ","JA ",
 "JS ","JNS ","JP ","JNP ","JL ","JGE ","JLE ","JG ",
-
 ["ADD ","OR ","ADC ","SBB ","AND ","SUB ","XOR ","CMP "],
 ["ADD ","OR ","ADC ","SBB ","AND ","SUB ","XOR ","CMP "],
-
 invalid,
-
 ["ADD ","OR ","ADC ","SBB ","AND ","SUB ","XOR ","CMP "],
-
-"TEST ","TEST ",
-"XCHG ","XCHG ",
+"TEST ","TEST ","XCHG ","XCHG ",
 "MOV ","MOV ","MOV ","MOV ","MOV ",
-"LEA ",
-"MOV ",
-
+"LEA ","MOV ",
 ["POP ",invalid,invalid,invalid,invalid,invalid,invalid,invalid],
-
-"XCHG ","","","","","","","",
-
+"XCHG ",,,,,,,,
 [invalid,"CBW","CWDE","CQE"],
 [invalid,"CWD","CDQ","CQO"],
-invalid,
-
-"wait",
-
+invalid,"wait",
 [invalid,"PUSHF","PUSHFQ","PUSHFQ"],
 [invalid,"POPF","POPFQ","POPFQ"],
-
 "SAHF","LAHF",
-
-"MOV ","MOV ",
-"MOV ","MOV ",
-
+"MOV ","MOV ","MOV ","MOV ",
 "MOVS ","MOVS ",
 "CMPS ","CMPS ",
 "TEST ","TEST ",
 "STOS ","STOS ",
 "LODS ","LODS ",
 "SCAS ","SCAS ",
-
-"MOV ","","","","","","","",
-"MOV ","","","","","","","",
-
+"MOV ",,,,,,,,"MOV ",,,,,,,,
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
-
 "RET ","RET",
 invalid,invalid,
-
 ["MOV ",invalid,invalid,invalid,invalid,invalid,invalid,invalid],
 ["MOV ",invalid,invalid,invalid,invalid,invalid,invalid,invalid],
-
-"ENTER ","LEAVE",
-"RETF ","RETF",
-"INT 3","INT ","INTO",
-[invalid,"IRET","IRETD","IRETQ"], //IRET size names
-
+"ENTER ","LEAVE","RETF ","RETF","INT 3","INT ","INTO",
+[invalid,"IRET","IRETD","IRETQ"],
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
-
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
 ["ROL ","ROR ","RCL ","RCR ","SHL ","SHR ","SAL ","SAR "],
-
-invalid,invalid,invalid,
-
-"XLAT ",
-
-fpu,fpu,fpu,fpu,fpu,fpu,fpu,fpu, //float point unit
-
+invalid,invalid,invalid,"XLAT ",
+//*****************************************************************************************************
+//float point unit
+//*****************************************************************************************************
+["FADD ","FMUL ","FCOM ","FCOMP ","FSUB ","FSUBR ","FDIV ","FDIVR ",[]],
+["FLD ",InvalidFPU,"FST ","FSTP ","FLDENV ","FLDCW ","FNSTENV ","FNSTCW ",[]],
+["FIADD ","FIMUL ","FICOM ","FICOMP ","FISUB ","FISUBR ","FIDIV ","FIDIVR ",[]],
+["FILD ","FISTTP ","FIST ","FISTP ",InvalidFPU,"FLD ",InvalidFPU,"FSTP ",[]],
+["FADD ","FMUL ","FCOM ","DCOMP ","FSUB ","FSUBR ","FDIV ","FDIVR ",[]],
+["FLD ","FISTTP ","FST ","FSTP ","FRSTOR ",InvalidFPU,"FNSAVE ","FNSTSW ",[]],
+["FIADD ","FIMUL ","FICOM ","FICOMP ","FISUB ","FISUBR ","FIDIV ","FIDIVR ",[]],
+["FILD ","FISTTP ","FIST ","FISTP ","FBLD ","FILD ","FBSTP ","FISTP ",[]],
+//*****************************************************************************************************
+//end of float point unit instructions
+//*****************************************************************************************************
 "LOOPNE ","LOOPE ","LOOP ","JRCXZ ",
-
 "IN ","IN ","OUT ","OUT ",
-
 "CALL ","JMP ",
-
-invalid,
-
-"JMP ",
+invalid,"JMP ",
 "IN ","IN ","OUT ","OUT ",
-
 "LOCK ",
-
 "THIS IS AN UNUSED OP-CODE IT DOES NOTHING IT MIGHT DO SOMETHING IN NEW PROCESSORS TO COME",
-
-"REPNE ","REP ",
-
-"HLT","CMC",
-
+"REPNE ","REP ","HLT","CMC",
 ["TEST ",invalid,"NOT ","NEG ","MUL ","IMUL ","DIV ","IDIV "],
 ["TEST ",invalid,"NOT ","NEG ","MUL ","IMUL ","DIV ","IDIV "],
-
 "CLC","STC","CLI","CTI","CLD","STD",
-
 ["INC ","DEC ",invalid,invalid,invalid,invalid,invalid,invalid],
-
 ["INC ","DEC ","CALL ","CALL ","JMP ","JMP ","PUSH ",invalid]
 ];
+
+//*****************************************************************************************************
+//The other FPU operations for C0 and up
+//*****************************************************************************************************
+
+opcodes[0xD8][8][0x00]="FADD ";opcodes[0xD8][8][0x08]="FMUL ";opcodes[0xD8][8][0x10]="FCOM ";
+opcodes[0xD8][8][0x18]="FCOMP ";opcodes[0xD8][8][0x20]="FSUB ";opcodes[0xD8][8][0x28]="FSUBR ";
+opcodes[0xD8][8][0x30]="FDIV ";opcodes[0xD8][8][0x38]="FDIVR ";
+opcodes[0xD9][8][0x00]="FLD ";opcodes[0xD9][8][0x08]="FXCH ";opcodes[0xD9][8][0x10]="FNOP ";
+opcodes[0xD9][8][0x18]="FSTP1 ";opcodes[0xD9][8][0x20]="FCHS";opcodes[0xD9][8][0x21]="FABS";
+opcodes[0xD9][8][0x24]="FTST";opcodes[0xD9][8][0x25]="FXAM";opcodes[0xD9][8][0x28]="FLD1";
+opcodes[0xD9][8][0x29]="FLDL2T";opcodes[0xD9][8][0x2A]="FLDL2E";opcodes[0xD9][8][0x2B]="FLDPI";
+opcodes[0xD9][8][0x2C]="FLDLG2";opcodes[0xD9][8][0x2D]="FLDLN2";opcodes[0xD9][8][0x2E]="FLDZ";
+opcodes[0xD9][8][0x30]="F2XM1";opcodes[0xD9][8][0x31]="FYL2X";opcodes[0xD9][8][0x32]="FPTAN";
+opcodes[0xD9][8][0x33]="FPATAN";opcodes[0xD9][8][0x34]="FXTRACT";opcodes[0xD9][8][0x35]="FPREM1";
+opcodes[0xD9][8][0x36]="FDECSTP";opcodes[0xD9][8][0x37]="FINCSTP";opcodes[0xD9][8][0x38]="FPREM";
+opcodes[0xD9][8][0x39]="FYL2XP1";opcodes[0xD9][8][0x3A]="FSQRT";opcodes[0xD9][8][0x3B]="FSINCOS";
+opcodes[0xD9][8][0x3C]="FRNDINT";opcodes[0xD9][8][0x3D]="FSCALE";opcodes[0xD9][8][0x3E]="FSIN";
+opcodes[0xD9][8][0x3F]="FCOS";
+opcodes[0xDA][8][0x00]="FCMOVB ";opcodes[0xDA][8][0x08]="FCMOVE ";opcodes[0xDA][8][0x10]="FCMOVBE ";
+opcodes[0xDA][8][0x18]="FCMOVU ";opcodes[0xDA][8][0x29]="FUCOMPP";
+opcodes[0xDB][8][0x00]="FCMOVNB ";opcodes[0xDB][8][0x08]="FCMOVNE ";opcodes[0xDB][8][0x10]="FCMOVNBE ";
+opcodes[0xDB][8][0x18]="FCMOVNU ";opcodes[0xDB][8][0x20]="FENI";opcodes[0xDB][8][0x21]="FDISI";
+opcodes[0xDB][8][0x22]="FNCLEX";opcodes[0xDB][8][0x23]="FNINIT";opcodes[0xDB][8][0x24]="FSETPM";
+opcodes[0xDB][8][0x28]="FUCOMI ";opcodes[0xDB][8][0x30]="FCOMI ";
+opcodes[0xDC][8][0x00]="FADD ";opcodes[0xDC][8][0x08]="FMUL ";opcodes[0xDC][8][0x10]="FCOM2 ";
+opcodes[0xDC][8][0x18]="FCOMP3 ";opcodes[0xDC][8][0x20]="FSUBR ";opcodes[0xDC][8][0x28]="FSUB ";
+opcodes[0xDC][8][0x30]="FDIVR ";opcodes[0xDC][8][0x38]="FDIV ";
+opcodes[0xDD][8][0x00]="FFREE ";opcodes[0xDD][8][0x08]="FXCH4 ";opcodes[0xDD][8][0x10]="FST ";
+opcodes[0xDD][8][0x18]="FSTP ";opcodes[0xDD][8][0x20]="FUCOM ";opcodes[0xDD][8][0x28]="FUCOMP ";
+opcodes[0xDE][8][0x00]="FADDP ";opcodes[0xDE][8][0x08]="FMULP ";opcodes[0xDE][8][0x10]="FCOMP5 ";
+opcodes[0xDE][8][0x19]="FCOMPP";opcodes[0xDE][8][0x20]="FSUBRP ";opcodes[0xDE][8][0x28]="FSUBP ";
+opcodes[0xDF][8][0x00]="FFREEP ";opcodes[0xDF][8][0x08]="FXCH7 ";opcodes[0xDF][8][0x10]="FSTP8 ";
+opcodes[0xDF][8][0x18]="FSTP9 ";opcodes[0xDF][8][0x20]="FNSTSW AX";opcodes[0xDF][8][0x28]="FUCOMIP ";
+opcodes[0xDF][8][0x30]="FCOMIP ";
 
 //*****************************************************************************************************
 //operand decode settings
@@ -162,14 +156,8 @@ var OpcodeOperandType=[
 0x86,0x218DC4E,
 0x81,0x204DC4E,
 0x244C1,0x244CE,0x14322,0x15D22,
-
-//relative position conditional jumps
-
 0x91,0x91,0x91,0x91,0x91,0x91,0x91,0x91,
 0x91,0x91,0x91,0x91,0x91,0x91,0x91,0x91,
-
-//end of jumps
-
 [0x10261,0x10261,0x10261,0x10261,0x10261,0x10261,0x10261,0x10261],
 [0x10C6E,0x10C6E,0x10C6E,0x10C6E,0x10C6E,0x10C6E,0x10C6E,0x10C6E],
 0x00,
@@ -179,83 +167,95 @@ var OpcodeOperandType=[
 0x8261,0x9C6E,
 0xC241,0xDC4E,
 0x8062,0xC04E,0xC440,
-
 [0x6A,0,0,0,0,0,0,0,0],
-
 0x5CEE,0x5CEE,0x5CEE,0x5CEE,0x5CEE,0x5CEE,0x5CEE,0x5CEE,
-0,0,
-0,0,
-0,0,
-0,0,
-
+0,0,0,0,0,0,0,0,
 0xE2E1,0xFCEE, //moffs format
 0x1C271,0x1DC7E, //moffs format
-
-//scan types
-
 0x142C1,0x15CCE,
 0x182A1,0x19CAE,
 0x102E1,0x10CEE,
 0xC1,0xCE,0xA1,0xAE,0xC1,0xCE,
-
-//end of scan types
-
-//register=number instructions technically MOV REG,IMM
-
 0x10221,0x10221,0x10221,0x10221,0x10221,0x10221,0x10221,0x10221,
 0x11C2E,0x11C2E,0x11C2E,0x11C2E,0x11C2E,0x11C2E,0x11C2E,0x11C2E,
-
-//end of MOV REG,IMM
-
 [0x10261,0x10261,0x10261,0x10261,0x10261,0x10261,0x10261,0x10261],
-
 [0x1026E,0x1026E,0x1026E,0x1026E,0x1026E,0x1026E,0x1026E,0x1026E],
-
-0x82,0,
-
-0,0,
-
+0x82,0,0,0,
 [0x10261,0,0,0,0,0,0,0],
 [0x10C6E,0,0,0,0,0,0,0],
-
 0x10481,0,0x82,0,
 0,0x81,0,0,
-
 [0x2C061,0x2C061,0x2C061,0x2C061,0x2C061,0x2C061,0x2C061,0x2C061],
 [0x2C06E,0x2C06E,0x2C06E,0x2C06E,0x2C06E,0x2C06E,0x2C06E,0x2C06E],
 [0x20261,0x20261,0x20261,0x20261,0x20261,0x20261,0x20261,0x20261],
 [0x2026E,0x2026E,0x2026E,0x2026E,0x2026E,0x2026E,0x2026E,0x2026E],
-
-0,0,0,
-
-0,
-
-0,0,0,0,0,0,0,0,
-
+0,0,0,0,
+//*****************************************************************************************************
+//float point unit
+//*****************************************************************************************************
+[
+//M-REAL
+4,4,4,4,4,4,4,4,
+//ST
+0,0,1,1,0,0,0,0
+],
+[
+//M-REAL
+4,,4,4,0,2,0,2,
+//ST
+1,1,,1,,,,
+],
+[
+//M-REAL
+4,4,4,4,4,4,4,4,
+//ST
+0,0,0,0,,,,
+],
+[
+//M-REAL
+4,4,4,4,,16,,16,
+//ST
+0,0,0,0,,0,0,
+],
+[
+//M-REAL
+8,8,8,8,8,8,8,8,
+//ST
+2,2,1,1,2,2,2,2
+],
+[
+//M-REAL
+8,8,8,8,0,,0,2,
+//ST
+1,1,1,1,1,1,,
+],
+[
+//M-REAL
+2,2,2,2,2,2,2,2,
+//ST
+2,2,1,,2,2,2,2
+],
+[
+//M-REAL
+2,2,2,2,16,8,16,8,
+//ST
+1,1,1,1,,0,0,
+],
+//*****************************************************************************************************
+//end of float point unit instructions
+//*****************************************************************************************************
 0x91,0x91,0x91,0x91,
-
 0x102E1,0x102EE,
-
 0x1C281,0x1DC81,
-
 0x94,0x94,0,0x91,
-
 0x244E1,0x244EE,
 0x1C322,0x1DD22,
-
-0,0,
-0,0,
-0,0,
-
+0,0,0,0,0,0,
 [0x10261,0,0x61,0x61,0xC2E1,0x61,0xC2E1,0x61],
 [0x10C6E,0,0x6E,0x6E,0xDCEE,0x6E,0xDCEE,0xDCEE],
-
 0,0,0,0,0,0,
-
 [0x61,0x61,0,0,0,0,0,0],
-
-[0x6E,0x6E,0x6A,0,0x6A,0,0x6A,0]
-];
+[0x6E,0x6E,0x6A,0,0x6A,0,0x6A,0]];
 
 //********************************registers and position in binary code********************************
 
@@ -276,16 +276,15 @@ var InstructionPos=-1; //used to show the 64 bit address that the instruction is
 
 PTRS=["[","BYTE PTR [","WORD PTR [","DWORD PTR [","QWORD PTR ["];
 
-//no ptr if size is set 0000 to allow operations with no prt such as LEA
-
 //define the different registers by selection number and REX extend
 
-REG=[["ES","CS","SS","DS","FS","GS","st(-2)","st(-1)","ES","CS","SS","DS","FS","GS","st(-2)","st(-1)"],
+REG=[["ES","CS","SS","DS","FS","GS","st(-2)","st(-1)","ES","CS","SS","DS","FS","GS","ST(-2)","ST(-1)"],
 [["AL","CL","DL","BL","AH","CH","DH","BH","R8B","R9B","R10B","R11B","R12B","R13B","R14B","R15B"],
 ["AL","CL","DL","BL","SPL","BPL","SIL","DIL","R8B","R9B","R10B","R11B","R12B","R13B","R14B","R15B"]],
 ["AX","CX","DX","BX","SP","BP","SI","DI","R8W","R9W","R10W","R11W","R12W","R13W","R14W","R15W"],
 ["EAX","ECX","EDX","EBX","ESP","EBP","ESI","EDI","R8D","R9D","R10D","R11D","R12D","R13D","R14D","R15D"],
-["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI","R8","R9","R10","R11","R12","R13","R14","R15"]];
+["RAX","RCX","RDX","RBX","RSP","RBP","RSI","RDI","R8","R9","R10","R11","R12","R13","R14","R15"],
+["ST","ST(0)","ST(1)","ST(2)","ST(3)","ST(4)","ST(5)","ST(6)","ST(7)"]];
 
 //SIB byte scale
 
@@ -511,7 +510,7 @@ output+="+"+REG[RamReg][SIB[1]+IndexExtend]+scale[SIB[0]];
 else
 {
 output+=REG[RamReg][ModR_MByte[2]+BaseExtend];
-};
+}
 
 if(ModR_MByte[0]==1){output+="+"+ReadInput(1);}else if(ModR_MByte[0]==2){output+="+"+ReadInput(4);}
 
@@ -554,7 +553,9 @@ return([Mode,O,RM]);}
 
 function Decode(Data)
 {
-//check if InstructionPos is reset then set it for current instruction
+var Name="",type=0,out=[];
+
+//check if InstructionPos is rest then set it for curnt instruction
 //only if ShowInstructionPos decoding is active
 
 if(InstructionPos==-1&ShowInstructionPos){InstructionPos=Pos;}
@@ -580,6 +581,11 @@ if(value==0x66){OvOperands=true;return("");}
 if(value==0x67){OvRam=true;return("");}
 if(value>=0x40&value<=0x4F){Rex=[value&0x01,(value&0x02)>>1,(value&0x04)>>2,(value&0x08)>>3,1];return("");}
 if(value==0xF0|value==0xF2|value==0xF3){Prefix=opcodes[value];return("");}
+
+//*******************************Normal instruction Decode if it is not an FPU instruction*****************************
+
+if(!(value>=0xD8&value<=0xDF))
+{
 
 //get the opcode
 
@@ -621,7 +627,7 @@ if(((type>>5)&0x0F)==2|((type>>14)&0x0F)==2|((type>>23)&0x0F)==2)
 Rex[4]=[0,0,0,0,0];OvRam=0;OvOperands=0;Name=Prefix+Name;Prefix="";
 InstructionPos=-1;HexCode="";
 
-//return unknown
+//return unkowen
 
 return("???\r\n");
 }
@@ -726,6 +732,87 @@ out=rm;
 //XCHG EAX,EAX should be NOP as it does no operation because XCHG EAX,EAX does not change the value of the acumulator
 
 if(value==0x90){Name="NOP";out="";}
+
+}
+
+//**************************else it is an float point unit instruction**************************
+
+else
+{
+//record which float point opcode group
+
+var FPUGroup=value;
+
+value=Data[Pos];
+
+//read the ModRM byte for MReal address format
+
+var ModRMByte=DecodeModRMByte(value);
+
+//get FPU operand type if not Mode 11
+
+if(ModRMByte[0]!=3)
+{
+type=OpcodeOperandType[FPUGroup][ModRMByte[1]];
+
+Name=opcodes[FPUGroup][ModRMByte[1]];
+
+if(typeof(type)!="undefined")
+{
+if(type<=15)
+{
+out=DecodeModRMAddress(ModRMByte,type);
+}
+else
+{
+out="TBYTE PTR "+DecodeModRMAddress(ModRMByte,0);
+}
+}
+
+}
+
+//else codes C0 and up
+
+else
+{
+
+type=OpcodeOperandType[FPUGroup][ModRMByte[1]+8];
+
+Name=opcodes[FPUGroup][8][(value&0x3F)];
+
+if(typeof(type)!="undefined")
+{
+
+Name=opcodes[FPUGroup][8][(value&0x38)];
+
+//ST,STi
+
+if(type==0)
+{
+out=REG[5][0]+","+REG[5][ModRMByte[2]+1];
+}
+
+//STi
+
+if(type==1)
+{
+out=REG[5][ModRMByte[2]+1];
+}
+
+//STi,ST
+
+if(type==2)
+{
+out=REG[5][ModRMByte[2]+1]+","+REG[5][0];
+}
+
+}
+
+}
+
+if(typeof(Name)=="undefined"){Name=InvalidFPU;}
+
+}
 
 //deactivate instruction overides if any after the instruction decodes
 
