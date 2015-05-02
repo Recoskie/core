686040fb44267d8c40254076939b44f0908cefb6
Damian Recoskie <damian_programmer@hotmail.com>
2015-05-02 11:32:58 -0400

    Added a Sample code.
    Added a Sample code for how to use the Decoder at the bottom of the code.

--- DisassembleX86-64.js
+++ DisassembleX86-64.js
@@ -2032,3 +2032,30 @@ function Disassemble(Code)
 
   return(Out);
 }
+
+//------------------------------------------------------------------------------------------------------------------------
+//Sample code
+//------------------------------------------------------------------------------------------------------------------------
+
+SetBasePosition("00007ffa417b5930"); //set the 64 bit address
+
+//Create a simple hex code
+
+var BinaryHex = "cceb004883c438c3cccccccccccccccc488bc448895810488970184889782055488da8e8fdffff4881ec10030000488b05930a0a004833c4488985000200004c8b05aad8090033c0488944244233f6c74424582a002c00488d05b23f05004889442460488d45f04889442448488bf9c744244000000802";
+
+//convert every tow hex digits into one byte pre array element
+
+var  ByteArray = new Array();
+
+for(var i = 0, el = 0;i < BinaryHex.length;i += 2, el++)
+{
+  ByteArray[el] = parseInt(BinaryHex.substring(i, i + 2), 16);
+}
+
+//used the byte code array with the Disassemble function
+
+var DisAsm = Disassemble(ByteArray);
+
+//Display the output
+
+alert(DisAsm);
