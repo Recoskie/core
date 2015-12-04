5c846e8cf7c61f1241cdad72cd9ea432f9e6f9e5
Damian Recoskie <damian_programmer@hotmail.com>
2015-12-04 10:25:44 -0500

    Update DisassembleX86-64.js

--- DisassembleX86-64.js
+++ DisassembleX86-64.js
@@ -160,6 +160,7 @@ var SIMD = 0;
 
 /*-------------------------------------------------------------------------------------------------------------------------
 The current Opcode.
+
 The lower 8 bits is the opcode the higher bits 9, and 10 are combined with an 8 bit code if an opcode expansion prefix is used.
 Normally Opcode is an 8 bit value 0 to 255 that uses the first byte opcode map. I will insert a comma to show how this numerically works.
 
@@ -172,13 +173,7 @@ If the 0F prefix is used the ninth binary bit is set starting at 256 with the 8 
 If the 0F 38 expansion is used bit 10 is set making opcode start at 512 plus the 8 bit opcode for 512 to 767.
 If the 0F 3A expansion is used but 10, and 9 are set making opcode start at 768 plus the 8 bit opcode for 768 to 1023.
 
-The opcode can be expanded to 9, or 10 bit's or higher to use the indexes further down in the Mnemonic array.
-
-The order the expansions go in the Mnemonic array is the same order the Map bits go in for EVEX.mm bits, and VEX.mmmmm.
-
-EVEX.mm=00 first byte opcode map, EVEX.mm = 01 two byte opcode map 0F.
-EVEX.mm=10 two byte opcode map 0F38, and EVEX.mm=11 two byte opcode map 0F 3A.
-
+EVEX.mm=00 first byte opcode map, EVEX.mm = 01 two byte opcode map 0F, EVEX.mm=10 two byte opcode map 0F 38, and EVEX.mm=11 two byte opcode map 0F 3A.
 EVEX.mm bits are set to bits 10, and 9 of the opcode.
 The Same is true with VEX.mmmmm which actually only uses only the two first bits for the same opcode maps.
 -------------------------------------------------------------------------------------------------------------------------*/
@@ -1188,7 +1183,7 @@ function DecodePrefixAdjustments()
     return(DecodePrefixAdjustments()); //restart function decode more prefix settings that can effect the decode instruction.
   }
 
-  //if 3A hex while using two byte opcode expansion with binary bit 9 using prefix code 3A set go three byte opcodes.
+  //if 3A hex while using two byte opcode expansion with binary bit 9 set using prefix code 3A go three byte opcodes.
 
   else if(Opcode == 0x13A)
   {
